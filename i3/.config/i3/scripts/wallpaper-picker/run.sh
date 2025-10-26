9c618e9cd9be2dad8f6a2c66c228b4209ee350ef
mcreinii <mcreinii@gmail.com>
2025-10-26 23:25:39 +0800

    Better...

--- i3/.config/i3/scripts/wallpaper-picker/run.sh
+++ i3/.config/i3/scripts/wallpaper-picker/run.sh
@@ -4,6 +4,12 @@ export PATH="$HOME/.local/bin:$PATH"
 WALLPAPER_DIR="$HOME/.wallpaper/"
 choice=$(ls "$WALLPAPER_DIR" | dmenu -i -p "Pick a wallpaper")
 
+ghostty_config() {
+  cat $HOME/.cache/wal/ghostty.conf > $HOME/.config/ghostty/config.theme
+  cat $HOME/.config/ghostty/config.theme > $HOME/.config/ghostty/config
+  cat $HOME/.config/ghostty/config.base >> $HOME/.config/ghostty/config
+}
+
 if [ -n "$choice" ]; then
   nitrogen --set-zoom-fill "$WALLPAPER_DIR/$choice" --save
 
@@ -14,6 +20,8 @@ if [ -n "$choice" ]; then
     notify-send "⚠️ wal backend failed — install haishoku."
     fi
   fi
+  
+  ghostty_config
 
   rm $HOME/.Xresources
   cp $HOME/.cache/wal/colors.Xresources $HOME/.Xresources
